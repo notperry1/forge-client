2715e2bb995107acea390a04fb97d7ef0037131e
RemainingToast <remainingtoast@gmail.com>
2020-07-11 02:32:12 +1000

    One step further...

--- src/main/java/ez/cloudclient/CloudClient/CloudClientMain.java
+++ src/main/java/ez/cloudclient/CloudClient/CloudClientMain.java
@@ -1,4 +1,5 @@
-package com.example.examplemod;
+package ez.cloudclient.CloudClient;
+
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,9 +8,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
+@Mod(modid = CloudClientMain.MODID, name = CloudClientMain.NAME, version = CloudClientMain.VERSION)
+public class CloudClientMain {
     public static final String MODID = "cloudclient";
     public static final String NAME = "Cloud Client";
     public static final String VERSION = "1.0";
