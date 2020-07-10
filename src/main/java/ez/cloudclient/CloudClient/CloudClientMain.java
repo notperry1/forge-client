67a880060c43f2584f1dd41c6ee3681f176d6ba3
RemainingToast <remainingtoast@gmail.com>
2020-07-11 01:17:06 +1000

    Gradle Stuff

--- src/main/java/ez/cloudclient/CloudClient/CloudClientMain.java
+++ src/main/java/ez/cloudclient/CloudClient/CloudClientMain.java
@@ -10,8 +10,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "cloudclient";
+    public static final String NAME = "Cloud Client";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -23,7 +23,10 @@ public class ExampleMod
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void init(FMLInitializationEvent event) {
+        System.out.println(NAME + " " + VERSION + " on Top.");
+    }
+
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
