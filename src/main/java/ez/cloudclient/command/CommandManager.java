78943bee2af18a92ed569691413fc4b0789650ae
RemainingToast <remainingtoast@gmail.com>
2020-07-11 22:42:11 +1000

    Folder refactor

--- src/main/java/ez/cloudclient/command/CommandManager.java
+++ src/main/java/ez/cloudclient/command/CommandManager.java
@@ -3,7 +3,7 @@ package ez.cloudclient.command;
 import java.util.ArrayList;
 
 public class CommandManager {
-    private ArrayList<ez.cloudclient.command.Command> commands;
+    private ArrayList<Command> commands;
 
     public CommandManager() {
         commands = new ArrayList<>();
