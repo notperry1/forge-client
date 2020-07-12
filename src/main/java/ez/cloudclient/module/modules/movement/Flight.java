cb9c90d3f72bb2e309aa8f650c35d0a45b7adaea
Zilleyy <coopercowley2004@gmail.com>
2020-07-12 18:58:06 +1000

    Added AntiHunger and Creative Flight.

---
+++ src/main/java/ez/cloudclient/module/modules/movement/Flight.java
@@ -0,0 +1,32 @@
+package ez.cloudclient.module.modules.movement;
+
+import ez.cloudclient.module.Module;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+public class Flight extends Module {
+    public Flight() {
+        super("Flight", Module.Category.COMBAT);
+    }
+
+    /*
+    * Added by Zilleyy 12/07/20
+    */
+
+    @Override
+    protected void onEnable() {
+        mc.player.capabilities.isFlying = true;
+    }
+
+    @Override
+    protected void onDisable() {
+        mc.player.capabilities.isFlying = false;
+    }
+
+    @SubscribeEvent
+    public void onUpdate(TickEvent event) {
+        if(mc.player != null) {
+                mc.player.capabilities.isCreativeMode = true;
+        }
+    }
+}
