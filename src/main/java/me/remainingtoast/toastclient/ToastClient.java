bda2a94abb61d6f4d9dd9e3e5163f79c0c9e5450
LittleDraily <63247691+LittleDraily@users.noreply.github.com>
2020-07-17 15:15:05 +0200

    eddited settings

--- src/main/java/me/remainingtoast/toastclient/ToastClient.java
+++ src/main/java/me/remainingtoast/toastclient/ToastClient.java
@@ -25,7 +25,7 @@ public class ToastClient {
     public static final String FULLVERSION = "b1";
     public static final String APP_ID = "669916916290420736";
     public static final String FULLNAME = NAME +" "+ FULLVERSION;
-    public static final String CLOUDCLIENT_CONFIGFILE = "ToastClientConfig.json";
+    public static final String CONFIGFILE = "ToastClientConfig.json";
     public static final Logger log = LogManager.getLogger("Toast Client");
     public static final CommandManager COMMAND_MANAGER = new CommandManager();
     public static final ModuleManager MODULE_MANAGER = new ModuleManager();
