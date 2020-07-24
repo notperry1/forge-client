77b4b8f15b6feeb7ec3b48a9653b36e8a9e15441
RemainingToast <remainingtoast@gmail.com>
2020-07-24 19:22:01 +1000

    Fixed hand render bug and added main hand and offhand rendering

--- src/main/java/me/remainingtoast/toastclient/module/modules/render/NameTags.java
+++ src/main/java/me/remainingtoast/toastclient/module/modules/render/NameTags.java
@@ -8,18 +8,22 @@ import me.remainingtoast.toastclient.util.NametagUtil;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.util.Comparator;
+import org.lwjgl.opengl.GL11;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -37,25 +41,20 @@ public class NameTags extends Module {
     @Property("Health")
     public boolean health = true;
 
+    private static FontRenderer font = mc.fontRenderer;
+
     @SubscribeEvent
     public void worldRender(RenderWorldLastEvent event){
-        if(mc.getRenderManager().options == null) return;
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableLighting();
-        GlStateManager.disableDepth();
-        mc.world.loadedEntityList.stream()
-                .filter(Entity::isEntityAlive)
-                .filter(entity -> (entity instanceof EntityPlayer))
-                .filter(entity -> mc.player.getDistance(entity) < range)
-                .sorted(Comparator.comparing(entity -> -mc.player.getDistance(entity)))
-                .forEach(this::drawNametag);
-        GlStateManager.disableTexture2D();
-        RenderHelper.disableStandardItemLighting();
-        GlStateManager.enableLighting();
-        GlStateManager.enableDepth();
+        for (EntityPlayer p : mc.world.playerEntities) {
+            if ((p != mc.getRenderViewEntity()) && (p.isEntityAlive())) {       
+                if (!p.getName().startsWith("Body #")) {
+                    drawNametag(p);
+                }
+            }
+        }
     }
 
-    private void drawNametag(Entity entity){
+    private void drawNametag(EntityPlayer entity){
         GlStateManager.pushMatrix();
         Vec3d vec3d = NametagUtil.getInterpolatedRenderPos(entity, mc.getRenderPartialTicks());
         float yAdd = entity.height + 0.5f - (entity.isSneaking() ? 0.25f : 0);
@@ -74,7 +73,7 @@ public class NameTags extends Module {
         float math = (dist / 8f) * (float) (Math.pow(1.258254f, this.scale));
         GlStateManager.scale(math, math, math);
 
-        FontRenderer font = mc.fontRenderer;
+
         GlStateManager.scale(-0.025F, -0.025F, 0.025F);
 
         String name = entity.getName() + (this.health ? " " + healthColoured(entity, Math.round(((EntityLivingBase) entity).getHealth() + (entity instanceof EntityPlayer ? ((EntityPlayer) entity).getAbsorptionAmount() : 0))) : "");
@@ -107,14 +106,106 @@ public class NameTags extends Module {
         if (!entity.isSneaking()) font.drawString(name, -length, 10, entity instanceof EntityPlayer ? 0xffffff : 0xffffff);
         else font.drawString(name, -length, 10, 0x00FF00);
 
+        int xOffset = 0;
+        for (ItemStack armourStack : entity.getArmorInventoryList()) {
+            if (armourStack != null) {
+                xOffset -= 8;
+            }
+        }
+
+        Object renderStack;
+        if (entity.getHeldItemMainhand() != null) {
+            xOffset -= 8;
+            renderStack = entity.getHeldItemMainhand().copy();
+            renderItem(entity, (ItemStack) renderStack, xOffset, -10);
+            xOffset += 16;
+        }
+        for (int index = 3; index >= 0; --index) {
+            ItemStack armourStack = entity.inventory.armorInventory.get(index);
+            if (armourStack != null) {
+                ItemStack renderStack1 = armourStack.copy();
+
+                renderItem(entity, renderStack1, xOffset, -10);
+                xOffset += 16;
+            }
+        }
+
+        ItemStack renderOffhand;
+        if (entity.getHeldItemOffhand() != null) {
+            xOffset -= 0;
+            renderOffhand = entity.getHeldItemOffhand().copy();
+
+            renderItem(entity, renderOffhand, xOffset, -10);
+            xOffset += 8;
+        }
+
         GlStateManager.glNormal3f(0.0F, 0.0F, 0.0F);
         glTranslatef(0, 20, 0);
 
         GlStateManager.scale(-40, -40, 40);
         GlStateManager.enableDepth();
         GlStateManager.popMatrix();
+
+
+
     }
 
+    private void renderItem(EntityPlayer player, ItemStack stack, int x, int y) {
+        GL11.glPushMatrix();
+        GL11.glDepthMask(true);
+        GlStateManager.clear(256);
+
+        GlStateManager.disableDepth();
+        GlStateManager.enableDepth();
+
+        net.minecraft.client.renderer.RenderHelper.enableStandardItemLighting();
+        mc.getRenderItem().zLevel = -100.0F;
+        GlStateManager.scale(1, 1, 0.01f);
+        mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x, (y / 2) - 12);
+        mc.getRenderItem().renderItemOverlays(mc.fontRenderer, stack, x, (y / 2) - 12);
+        mc.getRenderItem().zLevel = 0.0F;
+        GlStateManager.scale(1, 1, 1);
+        net.minecraft.client.renderer.RenderHelper.disableStandardItemLighting();
+        GlStateManager.enableAlpha();
+        GlStateManager.disableBlend();
+        GlStateManager.disableLighting();
+        GlStateManager.scale(0.5D, 0.5D, 0.5D);
+        GlStateManager.disableDepth();
+        renderEnchantText(player, stack, x, y - 18);
+        GlStateManager.enableDepth();
+        GlStateManager.scale(2.0F, 2.0F, 2.0F);
+        GL11.glPopMatrix();
+    }
+
+    private void renderEnchantText(EntityPlayer player, ItemStack stack, int x, int y) {
+        int encY = y - 24;
+        int yCount = encY - -5;
+        if (stack.getItem() instanceof ItemArmor || stack.getItem() instanceof ItemSword
+                || stack.getItem() instanceof ItemTool) {
+            font.drawStringWithShadow(stack.getMaxDamage() - stack.getItemDamage() + "\u00A74", x * 2 + 8, y + 26,
+                    -1);
+        }
+        NBTTagList enchants = stack.getEnchantmentTagList();
+        for (int index = 0; index < enchants.tagCount(); ++index) {
+            short id = enchants.getCompoundTagAt(index).getShort("id");
+            short level = enchants.getCompoundTagAt(index).getShort("lvl");
+            Enchantment enc = Enchantment.getEnchantmentByID(id);
+            if (enc != null) {
+                String encName = enc.isCurse()
+                        ? TextFormatting.RED
+                        + enc.getTranslatedName(level).substring(11).substring(0, 1).toLowerCase()
+                        : enc.getTranslatedName(level).substring(0, 1).toLowerCase();
+                encName = encName + level;
+                GL11.glPushMatrix();
+                GL11.glScalef(0.9f, 0.9f, 0);
+                font.drawStringWithShadow(encName, x * 2 + 13, yCount, -1);
+                GL11.glScalef(1f, 1f, 1);
+                GL11.glPopMatrix();
+                encY += 8;
+                yCount -= 10;
+            }
+        }
+    }
 
     private String healthColoured(Entity entity, int health){
         float max = ((EntityLivingBase) entity).getMaxHealth();
